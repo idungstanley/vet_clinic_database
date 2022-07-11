ea83f8a98351ba426498e8a13eddf45924ce30a5
sunday stanley Idung <sundaystanley56@gmail.com>
2022-07-12 00:40:51 +0100

    Add the schema, queries and data

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name like = '%mon';
+SELECT * from animals WHERE date_of_birth BETWEEN DATE '2016-01-01' and '2019-12-31'; 
+SELECT * from animals WHERE neutered is true and escape_attempts < 3;
+SELECT date_of_birth from animals WHERE name in ('Agumon', 'Pikachu');
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
+SELECT * from animals WHERE neutered is true;
+SELECT * from animals WHERE name != 'Gabumon';
+SELECT * from animals WHERE weight_kg between 10.3 and 17.4;
--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutered BOOL,
+    weight_kg DECIMAL
 );
