1150796956302decf69101b118144c4fe6eb5fa7
sunday stanley Idung <sundaystanley56@gmail.com>
2022-07-12 07:21:05 +0100

    Modified the query to extract only the name

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name like = '%mon';
-SELECT * from animals WHERE date_of_birth BETWEEN DATE '2016-01-01' and '2019-12-31'; 
+SELECT name from animals WHERE date_of_birth EXTRACT (year from date_of_birth) BETWEEN DATE 2016 and 2019; 
 SELECT * from animals WHERE neutered is true and escape_attempts < 3;
 SELECT date_of_birth from animals WHERE name in ('Agumon', 'Pikachu');
 SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
